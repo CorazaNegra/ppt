46147ae1b9c940ffa36218ceb4c23a25d6b5aa8f
Francisco Salar Pérez <frasalper@alu.edu.gva.es>
2023-03-20 19:55:43 +0100

    Rafactorización torito

--- src/MoveWords.java
+++ src/MoveWords.java
@@ -1,144 +1,148 @@
 import java.util.Random;
 
-public class MoveWords{
+public class MoveWords {
 
     public static final int EMPATE = 0;
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA", "LAGARTO", "SPOCK" };
-    private static final String[] validCommands = {"SALIR", "HELP"};
+    private static final String[] validMoves = { "TIJERAS", "PAPEL", "PIEDRA", "torito", "SPOCK" };
+    private static final String[] validCommands = { "SALIR", "HELP" };
 
     private Random rnd;
-    
-    public MoveWords(){
-    	rnd = new Random();
-    }
-    
-    public boolean isValidMoveCommand(String value){
 
-	    for (int i =0; i< validMoves.length; i++){
-	        if(validMoves[i].equals(value))
-	    	    return true;
-	    }
-	    
-	    for (int i =0; i< validCommands.length; i++){
-	        if(validCommands[i].equals(value))
-		        return true;
-	    }
-	    
-	    return false;
+    public MoveWords() {
+        rnd = new Random();
     }
 
-    public String randomMove(){
-	    float p = rnd.nextFloat();
-	    return validMoves[ (int) (p * validMoves.length)];
+    public boolean isValidMoveCommand(String value) {
+
+        for (int i = 0; i < validMoves.length; i++) {
+            if (validMoves[i].equals(value))
+                return true;
+        }
+
+        for (int i = 0; i < validCommands.length; i++) {
+            if (validCommands[i].equals(value))
+                return true;
+        }
+
+        return false;
     }
 
-    public void showMoves(){
+    public String randomMove() {
+        float p = rnd.nextFloat();
+        return validMoves[(int) (p * validMoves.length)];
+    }
+
+    public void showMoves() {
         for (String move : validMoves)
-	        System.out.print(move+" ");
-	    System.out.println();
-    }
-    public void showCommands(){
-	    for (String cmd : validCommands)
-	        System.out.print(cmd+" ");
-	    System.out.println();
+            System.out.print(move + " ");
+        System.out.println();
     }
 
-    public void showWords(){
+    public void showCommands() {
+        for (String cmd : validCommands)
+            System.out.print(cmd + " ");
+        System.out.println();
+    }
+
+    public void showWords() {
         showMoves();
-        showCommands();    
+        showCommands();
     }
 
-    private static int getIndex(String value){
-	    for (int i =0; i< validMoves.length; i++){
-	        if(validMoves[i].equals(value))
-		        return i;
-	    }
-    	return -1;
+    private static int getIndex(String value) {
+        for (int i = 0; i < validMoves.length; i++) {
+            if (validMoves[i].equals(value))
+                return i;
+        }
+        return -1;
     }
 
-    public static int checkWinner(String first, String second){
+    public static int checkWinner(String first, String second) {
         int first_i, second_i;
-        int resultado=0;
+        int resultado = 0;
 
         first_i = getIndex(first);
         second_i = getIndex(second);
 
-        if (first_i == second_i) return EMPATE;
-        
-        //*return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;
+        if (first_i == second_i)
+            return EMPATE;
 
-        switch(first){
-            case "PIEDRA" : 
-                if("PAPEL".equals(second)){
+        // *return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;
+
+        String string2 = "torito";
+        String string = string2;
+        switch (first) {
+            case "PIEDRA":
+                if ("PAPEL".equals(second)) {
                     return PIERDE;
                 }
-                if("TIJERAS".equals(second)){
+                if ("TIJERAS".equals(second)) {
                     return GANA;
                 }
-                if("LAGARTO".equals(second)){
+                if (string.equals(second)) {
                     return GANA;
                 }
-                if("SPOCK".equals(second)){
+                if ("SPOCK".equals(second)) {
                     return PIERDE;
                 }
-            case "PAPEL": 
-                if("PIEDRA".equals(second)){
+            case "PAPEL":
+                if ("PIEDRA".equals(second)) {
                     return PIERDE;
                 }
-                if("TIJERAS".equals(second)){
+                if ("TIJERAS".equals(second)) {
                     return PIERDE;
                 }
-                if("LAGARTO".equals(second)){
+                if (string.equals(second)) {
                     return PIERDE;
                 }
-                if("SPOCK".equals(second)){
+                if ("SPOCK".equals(second)) {
                     return GANA;
                 }
-            case "TIJERAS": 
-                if("PIEDRA".equals(second)){
+            case "TIJERAS":
+                if ("PIEDRA".equals(second)) {
                     return PIERDE;
                 }
-                if("PAPEL".equals(second)){
+                if ("PAPEL".equals(second)) {
                     return GANA;
                 }
-                if("LAGARTO".equals(second)){
+                if (string.equals(second)) {
                     return GANA;
                 }
-                if("SPOCK".equals(second)){
+                if ("SPOCK".equals(second)) {
                     return PIERDE;
                 }
-            case "LAGARTO": 
-                if("PIEDRA".equals(second)){
+            case "torito":
+                if ("PIEDRA".equals(second)) {
                     return PIERDE;
                 }
-                if("PAPEL".equals(second)){
+                if ("PAPEL".equals(second)) {
                     return GANA;
                 }
-                if("TIJERAS".equals(second)){
+                if ("TIJERAS".equals(second)) {
                     return PIERDE;
                 }
-                if("SPOCK".equals(second)){
+                if ("SPOCK".equals(second)) {
                     return GANA;
                 }
-            case "SPOCK": 
-                if("PIEDRA".equals(second)){
+            case "SPOCK":
+                if ("PIEDRA".equals(second)) {
                     return GANA;
                 }
-                if("PAPEL".equals(second)){
+                if ("PAPEL".equals(second)) {
                     return PIERDE;
                 }
-                if("TIJERAS".equals(second)){
+                if ("TIJERAS".equals(second)) {
                     return GANA;
                 }
-                if("LAGARTO".equals(second)){
+                if (string.equals(second)) {
                     return PIERDE;
                 }
         }
 
         return resultado;
     }
-	
-} 
+
+}
